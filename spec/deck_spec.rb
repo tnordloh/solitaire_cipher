1611349979abd5168ad116a5e3d3c583e49f98eb
Timothy Nordloh <tnordloh@gmail.com>
2015-11-23 14:03:06 -0700

    rename "letter" variable to "number", because we're looking at numbers, not letters.  "letter" is misleading in this case :)

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -5,9 +5,11 @@ require_relative "../lib/solitaire_cipher/deck"
 describe SolitaireCipher::Deck do
 
   it "finds the next letter" do
+    #test the examples from the ruby quiz, to ensure that I get the same answers
+    #as are specified in the quiz description.
     deck = SolitaireCipher::Deck.new()
-    [4,49,10,24,8,51,44,6,4,33].each do |letter|
-      deck.next_number.must_equal(letter)
+    [4,49,10,24,8,51,44,6,4,33].each do |number|
+      deck.next_number.must_equal(number)
     end
   end
 
